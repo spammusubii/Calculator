74561c0fbe7f9d9a9fa07ed9b4fe26753ea80b9c
Justin <194769941+spammusubii@users.noreply.github.com>
2025-02-24 19:18:14 -0800

    Create basic calculator functions Created basic functions that will allow users to add, subtract, multiply, and divide two numbers.

---
+++ main.js
@@ -0,0 +1,34 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+}
+
+let num1;
+let num2;
+let operator;
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "x":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return 0;
+    }
+}
